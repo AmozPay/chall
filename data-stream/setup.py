675333d2ff4143ea929fcaa224fb596c65c7344e
AmozPay <amoz.pay@epitech.eu>
2023-03-23 23:42:51 +0100

    Register triggers at the same time as transformations

--- data-stream/setup.py
+++ data-stream/setup.py
@@ -1,7 +1,12 @@
 import logging
-from gqlalchemy import Memgraph, MemgraphKafkaStream
+from gqlalchemy import Memgraph, MemgraphKafkaStream, MemgraphTrigger
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka.errors import TopicAlreadyExistsError, NoBrokersAvailable
+from gqlalchemy.models import (
+    TriggerEventType,
+    TriggerEventObject,
+    TriggerExecutionPhase,
+)
 from time import sleep
 
 
@@ -41,6 +46,8 @@ def run(memgraph, kafka_ip, kafka_port):
 
     topic_list = [
         NewTopic(name="prices", num_partitions=1, replication_factor=1),
+        NewTopic(name="best_rates", num_partitions=1, replication_factor=1),
+        NewTopic(name="best_routes", num_partitions=1, replication_factor=1),
     ]
 
     try:
@@ -56,5 +63,23 @@ def run(memgraph, kafka_ip, kafka_port):
         transform="exchange.prices",
         bootstrap_servers="'kafka:9092'",
     )
+
+    update_best_exchange = MemgraphTrigger(
+        name="update_best_exchange",
+        event_type=TriggerEventType.UPDATE,
+        event_object=TriggerEventObject.RELATIONSHIP,
+        execution_phase=TriggerExecutionPhase.AFTER,
+        statement=f"CALL kafka_producer.send_best_exchanges('{kafka_ip}:{kafka_port}');",
+    )
+
+    update_best_path = MemgraphTrigger(
+        name="update_best_path",
+        event_type=TriggerEventType.UPDATE,
+        event_object=TriggerEventObject.RELATIONSHIP,
+        execution_phase=TriggerExecutionPhase.AFTER,
+        statement=f"CALL kafka_producer.send_best_trade_paths('{kafka_ip}:{kafka_port}');",
+    )
+    memgraph.create_trigger(update_best_exchange)
+    memgraph.create_trigger(update_best_path)
     memgraph.create_stream(stream)
     memgraph.start_stream(stream)
